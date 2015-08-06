c8bdb0177f1ee2f5a62ff45d26bc85411cef43d2
Taras Dolynyuk <trsdln@gmail.com>
2015-08-06 11:32:28 +0300

    Greetings journal & external service simulation

--- main.js
+++ main.js
@@ -1,24 +1,48 @@
 //both
-
+GreentingsJournal = new Mongo.Collection('greetings_journal');
 
 //server
 if (Meteor.isServer) {
+  MyExternalEchoService = {
+    sayHelloToUser: function (userName, onResultCb) {
+      var processName = function () {
+        var helloMessage = 'Hello, ' + userName;
+        onResultCb(null, helloMessage);
+      };
+
+      setTimeout(processName, 1000);
+    }
+  };
+
   Meteor.methods({
     queryExternalService: function (userName) {
-      return 'Hello, ' + userName;
+      MyExternalEchoService.sayHelloToUser(userName, function (err, res) {
+        if (err) {
+          console.log(err)
+        } else {
+          GreentingsJournal.insert({message: res})
+        }
+      });
     }
   });
 }
 
 //client
 if (Meteor.isClient) {
+  Template.TestView.helpers({
+    greetings: function () {
+      return GreentingsJournal.find();
+    }
+  });
+
   Template.TestView.events({
     'click .send': function (event, tmpl) {
       var userName = tmpl.$('#user-name').val();
 
       Meteor.call('queryExternalService', userName, function (err, res) {
-        var response = err && err.toString() || res;
-        tmpl.$('.service-response').text(response)
+        if (err) {
+          alert(err)
+        }
       });
     }
   })
